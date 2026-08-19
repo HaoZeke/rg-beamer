1d39c0ca0bc62792f517ad482318990c3765b7a9
Rohit Goswami <rgoswami@ieee.org>
2026-08-18 20:53:01 -0500

    feat: Doom helper to register the rg-beamer org class

---
+++ emacs/rg-beamer.el
@@ -0,0 +1,54 @@
+;;; rg-beamer.el --- Load the rg Beamer theme from this checkout -*- lexical-binding: t; -*-
+
+;; Puts theme/ on TEXINPUTS and registers an org-latex class so
+;; #+LaTeX_CLASS: rg-beamer works from ox-beamer.
+
+;;; Commentary:
+
+;; In Doom, from config.org:
+;;
+;;   (load! (expand-file-name "Git/Github/TeX/rg-beamer/emacs/rg-beamer.el" (getenv "HOME")))
+;;
+;; The yasnippet in emacs/org-beamer-rg is a sibling of
+;; orgBeamerMetropolis. Copy or symlink it into
+;; ~/.config/doom/snippets/org-mode/.
+
+;;; Code:
+
+(defgroup rg-beamer nil
+  "HaoZeke Beamer theme helpers."
+  :group 'org-export)
+
+(defconst rg-beamer-root
+  (file-name-directory (directory-file-name (file-name-directory load-file-name)))
+  "Checkout root that contains theme/.")
+
+(defun rg-beamer--theme-dir ()
+  (expand-file-name "theme" rg-beamer-root))
+
+(defun rg-beamer-setup-texinputs ()
+  "Prepend the theme directory to TEXINPUTS for tectonic and xelatex."
+  (let* ((dir (file-name-as-directory (rg-beamer--theme-dir)))
+         (cur (or (getenv "TEXINPUTS") "")))
+    (unless (string-match-p (regexp-quote dir) cur)
+      (setenv "TEXINPUTS" (concat dir ":" cur)))))
+
+(defun rg-beamer-register-org-class ()
+  "Add `rg-beamer' to `org-latex-classes'."
+  (require 'ox-latex)
+  (unless (assoc "rg-beamer" org-latex-classes)
+    (add-to-list 'org-latex-classes
+                 '("rg-beamer"
+                   "\\documentclass[aspectratio=169,11pt]{beamer}\n\\usetheme{rg}"
+                   ("\\section{%s}" . "\\section*{%s}")
+                   ("\\subsection{%s}" . "\\subsection*{%s}")
+                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))))
+
+(rg-beamer-setup-texinputs)
+(with-eval-after-load 'ox-latex
+  (rg-beamer-register-org-class))
+(with-eval-after-load 'ox-beamer
+  (rg-beamer-register-org-class))
+
+(provide 'rg-beamer)
+;;; rg-beamer.el ends here
